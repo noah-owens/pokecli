a110535f3b9226025e552a39e580715c68deeeca
noah-owens <lvl7cleric@gmail.com>
2025-09-16 12:42:21 -0600

    Modify Pokemon type struct

--- api/types.go
+++ api/types.go
@@ -1,32 +1,26 @@
 package api
 
 type Pokemon struct {
-	ID             int    `json:"id"`
-	Name           string `json:"name"`
-	BaseExperience int    `json:"base_experience"`
-	Height         int    `json:"height"`
-	IsDefault      bool   `json:"is_default"`
-	Order          int    `json:"order"`
-	Weight         int    `json:"weight"`
-	Abilities      []struct {
-		IsHidden bool `json:"is_hidden"`
-		Slot     int  `json:"slot"`
-		Ability  struct {
+	Name   string `json:"name"`
+	ID     int    `json:"id"`
+	Height int    `json:"height"`
+	Weight int    `json:"weight"`
+	Order  int    `json:"order"`
+
+	Abilities []struct {
+		Ability struct {
 			Name string `json:"name"`
 			URL  string `json:"url"`
 		} `json:"ability"`
+		IsHidden bool `json:"is_hidden"`
+		Slot     int  `json:"slot"`
 	} `json:"abilities"`
+
 	Forms []struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"forms"`
-	GameIndices []struct {
-		GameIndex int `json:"game_index"`
-		Version   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"version"`
-	} `json:"game_indices"`
+
 	HeldItems []struct {
 		Item struct {
 			Name string `json:"name"`
@@ -40,29 +34,45 @@ type Pokemon struct {
 			} `json:"version"`
 		} `json:"version_details"`
 	} `json:"held_items"`
-	LocationAreaEncounters string `json:"location_area_encounters"`
-	Moves                  []struct {
+
+	Moves []struct {
 		Move struct {
 			Name string `json:"name"`
 			URL  string `json:"url"`
 		} `json:"move"`
 		VersionGroupDetails []struct {
-			LevelLearnedAt int `json:"level_learned_at"`
-			VersionGroup   struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version_group"`
+			LevelLearnedAt  int `json:"level_learned_at"`
 			MoveLearnMethod struct {
 				Name string `json:"name"`
 				URL  string `json:"url"`
 			} `json:"move_learn_method"`
-			Order int `json:"order"`
+			Order        any `json:"order"`
+			VersionGroup struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version_group"`
 		} `json:"version_group_details"`
 	} `json:"moves"`
+
+	PastAbilities []struct {
+		Abilities []struct {
+			Ability  any  `json:"ability"`
+			IsHidden bool `json:"is_hidden"`
+			Slot     int  `json:"slot"`
+		} `json:"abilities"`
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+	} `json:"past_abilities"`
+
+	PastTypes []any `json:"past_types"`
+
 	Species struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"species"`
+
 	Stats []struct {
 		BaseStat int `json:"base_stat"`
 		Effort   int `json:"effort"`
@@ -71,6 +81,7 @@ type Pokemon struct {
 			URL  string `json:"url"`
 		} `json:"stat"`
 	} `json:"stats"`
+
 	Types []struct {
 		Slot int `json:"slot"`
 		Type struct {
@@ -78,28 +89,4 @@ type Pokemon struct {
 			URL  string `json:"url"`
 		} `json:"type"`
 	} `json:"types"`
-	PastTypes []struct {
-		Generation struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"generation"`
-		Types []struct {
-			Slot int `json:"slot"`
-			Type struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"type"`
-		} `json:"types"`
-	} `json:"past_types"`
-	PastAbilities []struct {
-		Generation struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"generation"`
-		Abilities []struct {
-			Ability  any  `json:"ability"`
-			IsHidden bool `json:"is_hidden"`
-			Slot     int  `json:"slot"`
-		} `json:"abilities"`
-	} `json:"past_abilities"`
 }
